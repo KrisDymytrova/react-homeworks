7eca304d75a25eac8477b27d582e9c64b57bc4ed
KrisDymytrova <kris.kuznetsova1312@gmail.com>
2024-04-27 02:36:51 +0300

    HW 84 Add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Card from './components/Card'
+import CardClass from './components/CardClass'
+
 
 function App() {
     return (
         <div className="App">
             <Card />
+            <CardClass />
         </div>
     );
 }
